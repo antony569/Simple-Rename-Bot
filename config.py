56c245530f38460f8862bc89f40de956a7f297ed
antony569 <92138589+antony569@users.noreply.github.com>
2022-10-15 19:11:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "5196680"))
+API_HASH = environ.get("API_HASH", "62f50f770a47992cfc21ba9f443f2cea")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5771037813:AAEXSpf4jTv8yRQrB5IXyugoMviLzBVkmQY")
+ADMIN = int(environ.get("ADMIN", "1256120456"))          
+CAPTION = environ.get("CAPTION", "I am the Helper of BH Movie Zone")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/113ae620e81ef8ae76cbe.png")
